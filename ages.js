19ab9f78defffd537911fcb1ad9db6b818acbece
Azutine <aj45garcez@gmail.com>
2024-09-12 12:45:08 +0800

    first commit

---
+++ ages.js
@@ -0,0 +1,17 @@
+const age=20;
+
+if (age<= 20) {
+    console.log("You are an adult")
+}
+else if (age>= 60){
+    console.log ("senior")
+}
+else if (age<=18){
+    console.log ("minor")
+}else if (age= 0){
+    console.log ("baby")
+}else if (age>= 28){
+    console.log ("ready to mary")
+}
+else {console.log ("You are not a human");
+}
